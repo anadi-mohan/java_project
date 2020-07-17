74bc6b0c345df0ef679222524cb992a2beb3ea07
ANADI MOHAN <anadi@localhost.localdomain>
2020-07-17 02:28:18 -0700

    Project Setup Completed

--- Image_Compression/src/application/Main.java
+++ Image_Compression/src/application/Main.java
@@ -1,7 +1,9 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
@@ -11,11 +13,13 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("FirstJFX.fxml"));
-			Scene scene = new Scene(root,400,400);
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("FirstJFX.fxml"));
+            BorderPane root = (BorderPane)loader.load();
+			Scene scene = new Scene(root,800,600);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
+            FXController controller = loader.getController();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
